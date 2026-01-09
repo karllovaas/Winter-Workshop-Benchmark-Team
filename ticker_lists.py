60c8b63c2ebbc83bba1db4e52ab4d4fb6490c027
Karl B. Lovaas <55645862+karllovaas@users.noreply.github.com>
2026-01-09 16:11:56 -0600

    added ticker class

---
+++ ticker_lists.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import typing
+
+
+class getTickers():
+    def __init__(self) -> None:
+        '''
+        Load ticker via or Parquet 
+        '''
+    def get_russell_3000(self) -> pd.Series: 
+        return pd.Series() 
+
+    def get_russell_1000(self) -> pd.Series: 
+        return pd.Series()
+    
+    def get_russell_2000(self) -> pd.Series: 
+        return pd.Series() 
